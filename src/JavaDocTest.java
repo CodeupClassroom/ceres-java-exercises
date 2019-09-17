8563ae50dbc7ee8750c77c1416fba9bf925449bf
Justin Reich <justinreich.dev@gmail.com>
2019-09-17 08:45:49 -0500

    add Javadoc example and HelloWorld solutions

---
+++ src/JavaDocTest.java
@@ -0,0 +1,25 @@
+/** A really really cool class for doing cool things
+ * @author Justin
+ * @version 1.5
+ */
+public class JavaDocTest {
+
+    /** A typical main method
+     * @author Justin
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /** Add two operands together
+     *
+     * @param num1 the first number passed into the method
+     * @param num2 the second number passed into the method
+     * @return the sum of num1 and num2
+     */
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
