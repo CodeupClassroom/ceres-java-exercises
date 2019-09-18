6f9fdb9a07dc4c0cb215a17224d36109fee46cc8
Fernando Mendoza <fernandoza@gmail.com>
2019-09-18 10:57:19 -0500

    feat: finished all the exercises.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,53 +3,53 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        int i = 5;
-//        while(i <= 15){
-//            System.out.print(i + " ");
-//            i++;
-//        }
+        int i = 5;
+        while(i <= 15){
+            System.out.print(i + " ");
+            i++;
+        }
 
         int x = 0;
-//        do{
-//            System.out.println(x);
-//            x += 2;
-//        }while(x <= 100);
-//
-//        for(x = 0; x <= 100; x += 2){
-//            System.out.println(x);
-//        }
+        do{
+            System.out.println(x);
+            x += 2;
+        }while(x <= 100);
+
+        for(x = 0; x <= 100; x += 2){
+            System.out.println(x);
+        }
 
         x = 100;
-//        do{
-//            System.out.println(x);
-//            x -= 5;
-//        }while(x >= -10);
+        do{
+            System.out.println(x);
+            x -= 5;
+        }while(x >= -10);
 
-//        for(x = 100;x >= -10;x -= 5){
-//            System.out.println(x);
-//        }
+        for(x = 100;x >= -10;x -= 5){
+            System.out.println(x);
+        }
 
-//        long y = 2;
-//        do{
-//            System.out.println(y);
-//            y = y * y;
-//        }while(y <= 1000000);
+        long y = 2;
+        do{
+            System.out.println(y);
+            y = y * y;
+        }while(y <= 1000000);
 
-//        for(long y = 2; y <= 1000000; y = y * y){
-//            System.out.println(y);
-//        }
-//
-//        for(x = 1; x <= 100; x++){
-//            if((x % 3 == 0) && (x % 5 == 0)){
-//                System.out.println(x+"FizzBuzz");
-//            } else if(x % 3 == 0){
-//                System.out.println(x+"Fizz");
-//            } else if(x % 5 == 0){
-//                System.out.println(x+"Buzz");
-//            } else {
-//                System.out.println(x);
-//            }
-//        }
+        for(long y2 = 2; y2 <= 1000000; y2 = y2 * y2){
+            System.out.println(y2);
+        }
+
+        for(x = 1; x <= 100; x++){
+            if((x % 3 == 0) && (x % 5 == 0)){
+                System.out.println(x+"FizzBuzz");
+            } else if(x % 3 == 0){
+                System.out.println(x+"Fizz");
+            } else if(x % 5 == 0){
+                System.out.println(x+"Buzz");
+            } else {
+                System.out.println(x);
+            }
+        }
 
         String yesNo = "Y";
 
@@ -71,5 +71,29 @@ public class ControlFlowExercises {
 
         System.out.println("Ok, have a good day");
 
+        do {
+            System.out.println("Give me a grade number:");
+            int grade = Integer.parseInt(input.nextLine());
+
+            String gradeLetter = "";
+            if (grade <= 100 && grade >= 88) {
+                gradeLetter = "A";
+            } else if (grade <= 87 && grade >= 80) {
+                gradeLetter = "B";
+            } else if (grade <= 79 && grade >= 67) {
+                gradeLetter = "C";
+            } else if (grade <= 66 && grade >= 60) {
+                gradeLetter = "D";
+            } else if (grade <= 59 && grade >= 0) {
+                gradeLetter = "F";
+            }
+
+            System.out.println(gradeLetter);
+            System.out.println("Would you like to test another grade Y/N");
+
+        }while(input.nextLine().equalsIgnoreCase("y"));
+
     }
+
+
 }
