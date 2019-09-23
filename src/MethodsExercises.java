e8cb7bf6c0c2714358a48bc0d2b7285cff496deb
Justin Reich <justinreich.dev@gmail.com>
2019-09-23 11:05:42 -0500

    add solution to methods exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,15 +1,16 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MethodsExercises {
 
     public static void main(String[] args) {
 //        System.out.println("Please enter a number between 1 and 10");
-//        int userNum = getInt(1, 10);
+//        int userNum = getInteger(1, 10);
         //
     }
 
     // with recursion
-//    public static int getInt(int min, int max) {
+//    public static int getInteger(int min, int max) {
 //        Scanner sc = new Scanner(System.in);
 //        if (sc.hasNextInt()) {
 //            int userNum = sc.nextInt();
@@ -17,11 +18,11 @@ public class MethodsExercises {
 //                return userNum;
 //            } else {
 //                System.out.println("Invalid range");
-//                return getInt(min, max);
+//                return getInteger(min, max);
 //            }
 //        } else {
 //            System.out.println("Invalid Input!");
-//            return getInt(min, max);
+//            return getInteger(min, max);
 //        }
 //    }
 //
@@ -30,7 +31,7 @@ public class MethodsExercises {
 
 
     // ================== using a loop
-//    public static int getInt(int min, int max) {
+//    public static int getInteger(int min, int max) {
 //        Scanner sc = new Scanner(System.in);
 //        while (true) {
 //            System.out.println("Please enter a number between " + min + " and " + max);
@@ -50,7 +51,7 @@ public class MethodsExercises {
 
 
 // ================== using recursion
-//    public static int getInt(int min, int max) {
+//    public static int getInteger(int min, int max) {
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Please enter a number between " + min + " and " + max);
 //        if (sc.hasNextInt()) {
@@ -59,11 +60,11 @@ public class MethodsExercises {
 //                return userNum;
 //            } else {
 //                System.out.println("Number out of range!");
-//                return getInt(min, max);
+//                return getInteger(min, max);
 //            }
 //        } else {
 //            System.out.println("Invalid input!");
-//            getInt(min, max);
+//            getInteger(min, max);
 //        }
 //        return 0;
 //    }
@@ -86,7 +87,7 @@ public class MethodsExercises {
 
 
     // get valid int in range
-/*    public static int getInt(int min, int max) {
+/*    public static int getInteger(int min, int max) {
         while (true) {
             System.out.println("Please enter a number between " + min + " and " + max);
             int userNum = getValidInt();
@@ -113,42 +114,42 @@ public class MethodsExercises {
 //    }
 //
 //    // get valid int in range
-//    public static int getInt(int min, int max) {
+//    public static int getInteger(int min, int max) {
 //        System.out.println("Please enter a number between " + min + " and " + max);
 //        int userNum = getValidInt();
 //        if (userNum >= min && userNum <= max) {
 //            return userNum;
 //        } else {
 //            System.out.println("Number out of range!");
-//            return getInt(min, max);
+//            return getInteger(min, max);
 //        }
 //    }
 
 
 // ================== using method overloading with recursion
 
-//    // get valid int
-//    public static int getInt() {
-//        Scanner sc = new Scanner(System.in);
-//        if (sc.hasNextInt()) {
-//            return sc.nextInt();
-//        } else {
-//            System.out.println("Invalid input!");
-//            return getInt();
-//        }
-//    }
-//
-////    // get valid int in range
-//    public static int getInt(int min, int max) {
-//        System.out.println("Please enter a number between " + min + " and " + max);
-//        int userNum = getInt();
-//        if (userNum >= min && userNum <= max) {
-//            return userNum;
-//        } else {
-//            System.out.println("Number out of range!");
-//            return getInt(min, max);
-//        }
-//    }
+    // get valid int
+    public static int getInteger() {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            return sc.nextInt();
+        } else {
+            System.out.println("Invalid input!");
+            return getInteger();
+        }
+    }
+
+//    // get valid int in range
+    public static int getInteger(int min, int max) {
+        System.out.println("Please enter a number between " + min + " and " + max);
+        int userNum = getInteger();
+        if (userNum >= min && userNum <= max) {
+            return userNum;
+        } else {
+            System.out.println("Number out of range!");
+            return getInteger(min, max);
+        }
+    }
 
 
 
@@ -175,7 +176,48 @@ public class MethodsExercises {
     }
 
 
+    // Exercise 3
+    public static long calculateFactorial(int arg) {
+        long output = 1;
+        for (int i = 1; i <= arg; i += 1) {
+            output *= i;
+        }
+        return output;
+    }
 
+    public static void processFactorial() {
+        boolean willContinue;
+        do {
+            System.out.println("Enter a number between 1 and 10");
+            Scanner sc = new Scanner(System.in);
+            int userInt = getInteger(1, 10);
+            System.out.println("The factorial is: " + calculateFactorial(userInt));
+            System.out.println("Do you wish to enter another number to calculate?");
+            willContinue = sc.next().equals("y");
+        } while(willContinue);
+        System.out.println("Have a nice day!");
+    }
+
+    // Exercise 4
+    public static int rollDie(int sides) {
+        Random r = new Random();
+        return r.nextInt(sides) + 1;
+    }
+
+    public static String rollDice(int sides) {
+        return String.format("%d and %d", rollDie(sides), rollDie(sides));
+    }
+
+    public static void processDice() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please choose a number of sides for the dice");
+        int sides = getInteger(1, 50);
+        do {
+            System.out.println("Rolling dice...");
+            System.out.println("You rolled " + rollDice(sides));
+            System.out.println("Do you wish to roll again [y/n]?");
+        } while(sc.next().equals("y"));
+    }
 
 
 }
