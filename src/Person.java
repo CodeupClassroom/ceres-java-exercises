63ac2d0ac80ed3fbf2d9c5ad3fd310c7150142fc
Fernando Mendoza <fernandoza@gmail.com>
2019-09-24 11:44:08 -0500

    feat: finish the lec

--- src/Person.java
+++ src/Person.java
@@ -1,33 +1,28 @@
 import java.util.Scanner;
 
 public class Person {
-    static String heart = "is beating";    public static long worldPopulation = 7_500_000_000L; // class property
+    static String heart = "is beating";
+    public static long worldPopulation = 7_500_000_000L; // class property
 
     // define fields
-    public String firstName;
+    private String firstName;
     // visibility
-    public String lastName;
+    private String lastName;
 
     // define methods
     public String sayHello(){
         return String.format("Hello from %s %s!", firstName, lastName);
     }
 
-    public static void main(String[] args) {
-        System.out.println(Person.heart);
-        // The default constructor
-        Person fer = new Person();
-        Person.worldPopulation += 1;
-        // Assign values
-        fer.firstName = "Fer";
-        fer.lastName = "Mendoza";
-        System.out.println(fer.sayHello());
+    public Person(String fN, String lN){
+        handlePerson();
+        this.firstName = fN;
+        this.lastName = lN;
+    }
 
-
-        System.out.println("worldPopulation = " + worldPopulation);
-
-
-
+    private void handlePerson(){
+        worldPopulation += 1;
+        System.out.println("A new person instance has been created");
     }
 
 }
