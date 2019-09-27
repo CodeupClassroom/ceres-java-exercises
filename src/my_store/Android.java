e8efd1966022c02d7715644d379f41e4d825fbd3
Fernando Mendoza <fernandoza@gmail.com>
2019-09-27 09:51:00 -0500

    feat: create an example of inheritance and polymorph

---
+++ src/my_store/Android.java
@@ -0,0 +1,12 @@
+package my_store;
+
+import oop.Person;
+
+public class Android extends Phone {
+
+    public Android(double sS) {
+        // how the phone (Android) gets created
+        super(sS, "AndroidOS");
+    }
+
+}
