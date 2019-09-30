99c4dc35b208010b634c49db487406ae33be592d
Fernando Mendoza <fernandoza@gmail.com>
2019-09-30 11:05:59 -0500

    feat: add a complex example using Movie objs for a HashMap

--- src/CollectionLec.java
+++ src/CollectionLec.java
@@ -1,11 +1,15 @@
+import movies.Movie;
 import my_store.Iphone;
 import my_store.Phone;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CollectionLec {
 
     public static void main(String[] args) {
+
+        mapsExample();
 
         // Integer[] numbers = new Integer[1];
         ArrayList<Integer> numbers = new ArrayList<>();
@@ -59,6 +63,49 @@ public class CollectionLec {
 
         System.out.println(phones.get(1).getOS());
         System.out.println(phones.get(2).getOS());
+
+    }
+
+    public static void mapsExample(){
+        HashMap<Long, String> users = new HashMap<>();
+        users.put(1L, "fmendozaro");
+        users.put(2L, "rorsinger");
+        users.putIfAbsent(2L, "danielfryar");
+        users.put(9L, "fmendozaro");
+
+        for (String val : users.values()) {
+            System.out.println("val = " + val);
+        }
+
+        users.remove(9L);
+
+        for (Long key : users.keySet()) {
+            System.out.println("users = " + users.get(key));
+        }
+
+//        this doesn't break the execution of the program
+        users.remove(9L);
+
+        System.out.println("users.get(1L) = " + users.get(1L));
+        System.out.println("users.get(2L) = " + users.get(2L));
+        System.out.println("users.get(3L) = " + users.getOrDefault(3L, "no an existing user"));
+
+        System.out.println("users.containsKey(8L) = " + users.containsKey(8L));
+
+        System.out.println("users.containsValue(\"fmendozaro\") = " + users.containsValue("fmendozaro"));
+
+//tt7286456
+        HashMap<String, Movie> fmdb = new HashMap<>();
+
+        fmdb.put("tt7286456", new Movie("Joker", "Crime, Drama, Thriller"));
+        fmdb.put("tt7286457", new Movie("Joker II", "Crime, Drama"));
+
+        for (String id : fmdb.keySet()){
+            System.out.println("id = " + id);
+            System.out.println("getName = " + fmdb.get(id).getName());
+            System.out.println("getCategory = " + fmdb.get(id).getCategory());
+        }
+
 
     }
 }
